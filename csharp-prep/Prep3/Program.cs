791de3b482765fd38dfbbd6b0828c0dd5506b998
Enock Mukuna <muk24020@byui.edu>
2026-01-22 12:04:29 -0700

    Prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,15 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1,110);
+        int magicNumber = randomGenerator.Next(1, 111);
 
         int guess = -1;
 
         while (guess != magicNumber)
         {
-            Console.Write("What is your guess?: ");
+            Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
             if (magicNumber > guess)
@@ -27,8 +26,7 @@ class Program
             {
                 Console.WriteLine("You guessed it!");
             }
-        }
-        
-        
+
+        }                    
     }
 }
