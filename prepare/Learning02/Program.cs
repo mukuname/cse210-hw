685cc028baa7bc03df86c0758d17b440c8750550
Enock Mukuna <muk24020@byui.edu>
2026-01-27 10:49:38 -0700

    Completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Electrical Engineer";
+        job1._company = "Schneider";
+        job1._startYear = 2027;
+        job1._endYear = 2030;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Siemens";
+        job2._startYear = 2030;
+        job2._endYear = 2033;
+
+        Resume myResume = new Resume();
+        myResume._name = "Enock Mukuna";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
